33a89d712b7c02825fb02c2c58ab47cefed3bb8c
richyyjd <richyyjd@gmail.com>
2015-12-13 20:00:32 +0100

    Escena_2_GameOver_en_progreso

---
+++ BubbleShip/Assets/Scripts/Nivel_2/Disparo_de_Freezer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+using Random=UnityEngine.Random;
+
+public class Disparo_de_Freezer : MonoBehaviour {
+
+	public GameObject bubble;
+	public float timeLapsedLastFire = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLapsedLastFire += Time.deltaTime;
+
+		if (timeLapsedLastFire > 1) {
+			
+			timeLapsedLastFire = 0;
+			GameObject b = Instantiate (bubble, transform.position + new Vector3 (0, (float)1.8, 0), transform.rotation) as GameObject;
+		}
+	}
+}
---
+++ BubbleShip/Assets/Scripts/Nivel_2/Freezer_flotando.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Freezer_flotando : MonoBehaviour {
+
+	//public Vector3 direccion = new Vector3 (0f, 1f , 0);
+
+	public Vector2 speed = new Vector2(10,10);
+	private Vector2 movement = new Vector2(1,1);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (direccion * Time.deltaTime);
+		/*
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+		*/
+		movement = new Vector2(0f, 0.01f);
+		transform.Translate(movement);
+		/*
+		if (Input.GetKeyDown ("space")){
+			transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+		}
+		*/
+	}
+	/*
+	void FixedUpdate(){
+		// 5 - Move the game object
+		rigidbody2D.velocity = movement;
+		rigidbody2D.velo
+		//rigidbody2D.AddForce(movement);
+	}
+	*/
+}
