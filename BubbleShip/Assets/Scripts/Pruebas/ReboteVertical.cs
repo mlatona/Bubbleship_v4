cad9bcf84938e0a8400a448d7d10e296d6f7e312
richyyjd <richyyjd@gmail.com>
2015-12-16 17:49:52 +0100

    Nuclear Explosion

---
+++ BubbleShip/Assets/Scripts/Pruebas/ReboteVertical.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReboteVertical : MonoBehaviour {
+	
+	int direction = 1;
+
+	void Update(){
+
+		transform.Translate(0,direction*0.01f,0);
+
+		if (transform.position.y > 2) {
+			direction = -1;
+		}
+
+		if (transform.position.y < -1) {
+			direction = 1;
+		}
+	}
+}
