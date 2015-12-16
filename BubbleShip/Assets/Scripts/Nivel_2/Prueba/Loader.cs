6014b6698f8ee7b240f02539ee790c6bcc2e487f
richyyjd <richyyjd@gmail.com>
2015-12-16 15:58:25 +0100

    SplashScreen en proceso

---
+++ BubbleShip/Assets/Scripts/Nivel_2/Prueba/Loader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject gameManager;
+	public GameObject soundManager;
+
+	void Awake () {
+		//if (gameManager && GameManager.instance == null)
+		if (gameManager == null)
+			Instantiate(gameManager);
+		if (soundManager && SoundManager.instance == null)
+			Instantiate(soundManager);
+	}
+}
