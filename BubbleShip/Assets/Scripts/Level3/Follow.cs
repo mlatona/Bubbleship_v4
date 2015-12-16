5ce39f62e4649d4d7646e70b0d9740ec8d6334eb
richyyjd <richyyjd@gmail.com>
2015-12-16 17:21:19 +0100

    Little Follow script fixed

--- BubbleShip/Assets/Scripts/Level3/Follow.cs
+++ BubbleShip/Assets/Scripts/Level3/Follow.cs
@@ -5,15 +5,18 @@ public class Follow : MonoBehaviour {
 
 	GameObject followObject;
 	public string followTag;
+	public float decrementoCamara;
+	//private float tmp;
 
 	void Start(){
 		followObject = GameObject.FindGameObjectWithTag (followTag);
+		//tmp = followObject.transform.position.y - decrementoCamara;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(!isDestroyed(followObject.gameObject)){
-			float tmp = followObject.transform.position.y;
+			float tmp = followObject.transform.position.y - decrementoCamara;
 			gameObject.transform.position = new Vector3(gameObject.transform.position.x,tmp,gameObject.transform.position.z);
 		}
 	}
