814ad7c85aa98629df677fb85e4168440c2a59db
Marco <marco.latona92@gmail.com>
2016-01-25 16:37:56 +0100

    BossSpeeds

---
+++ BubbleShip/Assets/Scripts/Game/IA/Torre/Shooting.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Torre{
+	public class Shooting : IState {
+
+		readonly GameObject stateable;
+		FireCommand fireCommand;
+		float timeElapsed = 0;
+		float lastFire = 0;
+		float fireTime = 0.2f;
+		float updateRating = 1;
+
+		public Shooting(GameObject stateableParam){
+			stateable = stateableParam;
+			fireCommand = stateable.GetComponent<FireCommand> ();
+		}
+
+		#region IState implementation
+
+		public void updateState ()
+		{
+			if(lastFire>fireTime){
+				fireCommand.Run();
+				lastFire = 0;
+			}
+			timeElapsed += Time.deltaTime;
+			lastFire += Time.deltaTime;
+
+		}
+		public IState changeState ()
+		{
+			IState newState = null;
+			if (timeElapsed > updateRating) {
+				
+				timeElapsed = 0;
+				newState = new Thinking(stateable);
+			}
+			return newState;
+		}
+
+		#endregion
+
+
+
+	}
+}
---
+++ BubbleShip/Assets/Scripts/Game/IA/Torre/TorreEnemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Torre;
+
+
+public class TorreEnemy : MonoBehaviour {
+
+	Enemy enemyIA;
+
+	// Use this for initialization
+	void Start () {
+		enemyIA = new Enemy (new Moving(gameObject));
+	}
+
+	void Update(){
+		enemyIA.Update ();
+	}
+}
+
